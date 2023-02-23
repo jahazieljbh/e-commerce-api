c03dca1a7dbb762583a3001272560b9be38951dd
Jahaziel <jahaziel.hernandez.jbh@gmail.com>
2023-02-23 10:43:06 -0600

    se agrego y configuro la base de datos

---
+++ index.js
@@ -0,0 +1,17 @@
+
+import { PORT } from "./config/config.js";
+import { connectDatabase } from "./config/database.js";
+
+
+async function startServer() {
+  try {
+    await connectDatabase();
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error starting server: ${error.message}`);
+  }
+}
+
+startServer();
